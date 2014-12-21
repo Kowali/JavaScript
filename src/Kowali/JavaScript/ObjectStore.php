62a7a8094e62454bd0e93b9a70656bf3fca6c994
Thomas Hupkens <thomas@hupkens.be>
2014-12-21 13:38:37 +0100

    Things

--- src/Kowali/JavaScript/ObjectStore.php
+++ src/Kowali/JavaScript/ObjectStore.php
@@ -2,43 +2,121 @@
 
 class ObjectStore {
 
+    protected $dumpers = [];
+    protected $scripts = [];
     protected $variables = [];
 
-    public function addVariable($name, $value)
+    public function pushScript($path, $inline = false)
     {
-        $this->variables[$name] = $value;
-    }
-    public function getVariables($scope = 'window', $output_tags = true)
-    {
-        if($scope != 'window')
-        {
-            $output = "var {$scope} = " . json_encode($this->variables);
-        }
-        else
-        {
-            $output = '';
-            foreach($this->variables as $key => $value)
-            {
-                $converted = json_encode($value);
-                $output .= "window.{$key} = {$converted};";
-            }
-        }
+        $dumper = new ScriptDumper($path, $inline);
 
-        if($output_tags && !empty($output))
-        {
-            return "<script>{$output}</script>";
-        }
-
-        return $output;
+        array_push($this->dumpers, $dumper);
+        array_push($this->scripts, $dumper);
     }
 
-    public function convertToJson($object)
+    public function unshiftScript()
     {
-        if(is_object($object) && method_exists($object, 'toJson'))
-        {
-            return $object->toJson();
-        }
+        $dumper = new ScriptDumper($path, $inline);
 
-        return json_encode($object);
+        array_unshift($this->dumpers, $dumper);
+        array_unshift($this->scripts, $dumper);
     }
+
+    public function pushVariable($name, $value)
+    {
+        $dumper = new VariableDumper($path, $inline);
+
+        array_push($this->dumpers, $dumper);
+        array_push($this->variables, $dumper);
+    }
+
+    public function unshiftVariable($name, $value)
+    {
+        $dumper = new VariableDumper($path, $inline);
+
+        array_unshift($this->dumpers, $dumper);
+        array_unshift($this->variables, $dumper);
+    }
+
+
+    public function flushVariables()
+    {
+
+    }
+
+    public function flushScripts()
+    {
+
+    }
+
+    public function flush()
+    {
+
+    }
+
+    // public function enqueueVariable($name, $value)
+    // {
+
+    // }
+
+    // /**
+    //  * Ass a variable.
+    //  *
+    //  * @param  string $name
+    //  * @param  mixed  $value
+    //  * @return void
+    //  */
+    // public function addVariable($name, $value)
+    // {
+    //     $this->variables[$name] = $value;
+    // }
+
+    // /**
+    //  * Return the variables as JS code.
+    //  *
+    //  * @param  string $scope
+    //  * @param  boolean $output_tags
+    //  * @return string
+    //  */
+    // public function getVariables($scope = 'window', $output_tags = true)
+    // {
+    //     if($scope != 'window')
+    //     {
+    //         $output = "var {$scope} = " . json_encode($this->variables);
+    //     }
+    //     else
+    //     {
+    //         $output = '';
+    //         foreach($this->variables as $key => $value)
+    //         {
+    //             $converted = json_encode($value);
+    //             $output .= "window.{$key} = {$converted};";
+    //         }
+    //     }
+
+    //     if($output_tags && !empty($output))
+    //     {
+    //         return "<script>{$output}</script>";
+    //     }
+
+    //     return $output;
+    // }
+
+    // /**
+    //  * Convert an object to json
+    //  *
+    //  * @param  mixed $object
+    //  * @return string
+    //  */
+    // public function convertToJson($object)
+    // {
+    //     if(is_object($object) && method_exists($object, 'toJson'))
+    //     {
+    //         return $object->toJson();
+    //     }
+
+    //     return json_encode($object);
+    // }
+
+    // public function enqueueScript($path, )
 }
